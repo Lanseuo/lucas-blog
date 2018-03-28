4bcd4d54f9ba67e1d166264c913123c5461be89f
Lucas Hild <contact@lucas-hild.de>
2018-03-28 14:29:18 +0200

    Add post template

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-from livereload import Server, shell
+import os
+
+from flask import Flask, render_template, redirect, url_for
+from livereload import Server
 
 
 app = Flask(__name__)
@@ -8,6 +10,23 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/<string:permalink>")
+def post(permalink):
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    all_files = os.listdir(current_path + "/posts")
+
+    # If post doesn't exist
+    if not list(filter(lambda x: permalink in x, all_files)):
+        return redirect(url_for("index"))
+
+    file_name = list(filter(lambda x: permalink in x, all_files))[0]
+
+    with open(current_path + "/posts/" + file_name) as f:
+        content = f.read()
+
+    return render_template("post.html", title=permalink, content=content)
 
 
 if __name__ == "__main__":
