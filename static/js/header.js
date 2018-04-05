79c0b2d68882aec60fb4422f53d8243b8dd0ec67
Lucas Hild <contact@lucas-hild.de>
2018-04-05 18:33:27 +0200

    Update text of navbar on scrolling

--- static/js/header.js
+++ static/js/header.js
@@ -1,12 +1,17 @@
 $(window).ready(function() {
+  let headerTitle = $('#header-content h1 a').text();
+  console.log(headerTitle);
+
   $(window).scroll(function(event) {
     $('nav').css('position', 'fixed');
     if ($(window).scrollTop() > 100) {
       $('nav').addClass('visible');
       $('#header-content h1').addClass('go-to-navbar');
+      $('#header-content h1 a').text('Lucas Blog');
     } else {
       $('nav').removeClass('visible');
       $('#header-content h1').removeClass('go-to-navbar');
+      $('#header-content h1 a').text(headerTitle);
     }
   });
 });
